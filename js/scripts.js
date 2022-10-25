7ccb7ee091c6c76898c332e7d87ebae1f419fcfa
SonnyGitMoney <sendsonnymail@gmail.com>
2022-10-25 16:56:52 -0700

    Add wordTime function to js.
    Co-authored by: David Gamble <daviddgamble@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,35 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+function wordTime(sent) {
+  let textArray = sent.split(" ");
+  const newArray = [];
+  textArray.forEach(function(element1) {
+    let currentWord = element1;
+    let wordCount = 0;
+    textArray.forEach(function(element2, index) {
+      if (element1 === element2) {
+        wordCount++;
+        textArray.splice(index, 1)
+      }
+    })
+    newArray.push(currentWord + ': ' + wordCount);
+  })
+  return newArray;
+}
+
+
+/*Describe: wordTime()
+
+Test: "It should take array elements and list how many times used"
+Code: 
+const text = "face face heart face twelve heart niner feets";
+wordTime(text);
+Expected Output: ['face: 3', 'heart: 2', 'twelve: 1', 'niner: 1', 'feets: 1']
+
+*/
+
+
 // UI Logic
 
 function boldPassage(word, text) {
